cfc8298468ffb8ab11c0db627c834628c8af272b
PRIYANSHU SHARMA <131241998+Priyanshu7457@users.noreply.github.com>
2023-04-28 10:49:25 +0530

    Updated Script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variables and Constants 
 const input = document.getElementById("box");
 const answer = document.getElementById("answer");
 const selectBox1 = document.getElementById("selectbox1");
@@ -7,18 +8,16 @@ const reset = document.getElementById("reset");
 const HexInput = document.getElementById("Hexbox");
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
 const wrapper = document.createElement('div');
+let result;
 function isBinary(num) {
   while (num > 0) {
     if (num % 10 !== 0 && num % 10 !== 1) {
-      return false;
-    }
-    num = Math.floor(num / 10);
-  }
+      return false;}num = Math.floor(num / 10);}
   return true;
-}
-function isHexadecimal(str) 
-{ let hexRegex = /^[0-9A-Fa-f]+$/;
-                             return hexRegex.test(str);
+}function isHexadecimal(str) {
+  // Regular expression to match a valid hexadecimal string
+  let hexRegex = /^[0-9A-Fa-f]+$/;
+  return hexRegex.test(str);
 }
 function binaryToDecimal(binary) {
   let decimal = 0;
@@ -31,7 +30,6 @@ function binaryToDecimal(binary) {
   return decimal;
 }
 function binaryToHex(binary) {
-
   const decimal = parseInt(binary, 2);
   const hex = decimal.toString(16);
   return hex;
@@ -52,14 +50,18 @@ function octalToBinary(octal) {
   return parseInt(octal, 8).toString(2);
 
 }
-
 const isOctal = (c) => {
   let a = c.toString();
   let b = Array.from(a);
   for (let i = 0; i < b.length; i++) {
     if (b[i] >= 8) {
       appendAlert(`${c} Is Not A Octal Number`, 'danger', 'red');
-      return true;}}}
+      return true;
+    }
+
+
+  }
+}
 function octalToDecimal(octal) {
   return parseInt(octal, 8);
 
@@ -97,7 +99,6 @@ const appendAlert = (message, type, bColor, link) => {
     `   <div>${message}</div>`,
     '</div>'
   ].join('');
-
   alertPlaceholder.append(wrapper);
 }
 selectBox1.addEventListener("change", () => {
@@ -109,152 +110,167 @@ selectBox1.addEventListener("change", () => {
     HexInput.classList.add("H-hide");
     input.classList.remove("I-hide");
   }
-  let result;
-
-  convert.addEventListener("click", () => {
-    const unit1 = selectBox1.value;
-    const unit2 = selectBox2.value;
-    const inputValue = parseFloat(input.value);
-    const hexValue = HexInput.value; 
-     const done = () => {
-      input.style.border = `1px solid lime`;
-        HexInput.style.border='1px solid lime';
-    }
-    const binaryAlert = () => {
-      appendAlert(`${inputValue} Is Not A Binary Number`, 'danger', 'red');
-      return result = 'Output-Box';
-    }
- const hexaDecimalError = () => {
-    appendAlert(`${hexValue} Is Not A  Hexa-Decimal Number`, 'danger', 'red');
-    return result = 'Output-Box';
-    }
-    const hexaDecimalWarn = () => {
-      appendAlert(`Enter A Hexa-Decimal Number`, 'warning', 'coral');
-      return result = 'Output-Box';
-    }
-    if (unit1 != 4) {
-      if (isNaN(inputValue)) {
-        appendAlert('Please Enter A Number', 'warning', 'coral');
+  {
+    convert.addEventListener("click", () => {
+      const unit1 = selectBox1.value;
+      const unit2 = selectBox2.value;
+      const inputValue = parseFloat(input.value);
+      const hexValue = HexInput.value;
+      if ((unit1 == 0 && unit2 == 0) || (unit1 > 0 && unit2 == 0) || (unit1 == 0 && unit2 >= 0)) {appendAlert('Please Select A Type', 'warning', 'coral');
+        result = 'Output-Box'; }
+      const done = () => {
+        input.style.border = `1px solid lime`;
+        HexInput.style.border = '1px solid lime';
+      }
+      const binaryAlert = () => {
+        appendAlert(`${inputValue} Is Not A Binary Number`, 'danger', 'red');
         return result = 'Output-Box';
-      } else {
-        done();
       }
-    }
-    
-    if (unit1 === "1" && unit2 === "1") {
-      if (isBinary(inputValue)) {
-        result = inputValue;
-      } else {
-        binaryAlert();
+      const hexaDecimalError = () => {
+        appendAlert(`${hexValue} Is Not A  Hexa-Decimal Number`, 'danger', 'red');
+        return result = 'Output-Box';
       }
-    } else if (unit1 === "1" && unit2 === "2") {
-      if (isBinary(inputValue)) {
-        result = binaryToDecimal(inputValue);
-      } else {
-        binaryAlert();
+      const hexaDecimalWarn = () => {
+        appendAlert(`Enter A Hexa-Decimal Number`, 'warning', 'coral');
+        return result = 'Output-Box';
       }
-    } else if (unit1 === "1" && unit2 === "3") {
-      if (isBinary(inputValue)) {
-        result = decimalToOctal(parseInt(inputValue, 2));
-      } else {
-        binaryAlert();
-      }
-    } else if (unit1 === "1" && unit2 === "4") {
-      if (isBinary(inputValue)) {
-        result = binaryToHex(inputValue);
-      } else {
-        binaryAlert();
-      }
-    }
-    else if (unit1 === "2" && unit2 === "1") {
-      result = inputValue.toString(2).padStart(4, "0");
-    } else if (unit1 === "2" && unit2 === "2") {
-      result = inputValue;
-    } else if (unit1 === "2" && unit2 === "3") {
-      result = decimalToOctal(inputValue);
-    } else if (unit1 === "2" && unit2 === "4") {
-      result = decimalToHex(inputValue);
-    }
-    else if (unit1 === "3" && unit2 === "3") {
-      let a = isOctal(inputValue);
-      if (a == true) {
+      if (input.value.length > 16 || hexValue.length > 10) {
+        appendAlert('Value Length Is Too High', 'danger', 'red');
         result = 'Output-Box';
-      } else {
-        result = inputValue;
-      }
-    } else if (unit1 === "3" && unit2 === "1") {
-      let a = isOctal(inputValue);
-      if (a == true) {
-        result = 'Output-Box';
-      } else {
-        result = octalToBinary(inputValue);
-      }
-
-    } else if (unit1 === "3" && unit2 === "2") {
-      let a = isOctal(inputValue);
-      if (a == true) {
-        result = 'Output-Box';
-      } else {
-        result = octalToDecimal(inputValue);
-      }
-
-    } else if (unit1 === "3" && unit2 === "4") {
-
-      let a = isOctal(inputValue);
-      if (a == true) {
-        result = 'Output-Box';
-      } else {
-        result = octalToHex(inputValue);
-      }
-
-    }
-    else if (unit1 === "4" && unit2 === "1") {
-      console.log(hexValue);
-      if (isHexadecimal(hexValue)) {
-        done();
-        result = hexToBinary(hexValue);
-      } else if (hexValue <= 0) {
-        hexaDecimalWarn();
       }
       else {
-        hexaDecimalError();
+        if (unit1 != 4 && unit2 > 0) {
+          if (isNaN(inputValue)) {
+            appendAlert('Please Enter A Number', 'warning', 'coral');
+            return result = 'Output-Box';
+          }
+          else {
+            done();
+          }
+        } 
+        if (unit1 === "1" && unit2 === "1") {
+          if (isBinary(inputValue)) {
+            result = inputValue;
+          } else {
+            binaryAlert();
+          }
+        } else if (unit1 === "1" && unit2 === "2") {
+          if (isBinary(inputValue)) {
+            result = binaryToDecimal(inputValue);
+          } else {
+            binaryAlert();
+          }
+        } else if (unit1 === "1" && unit2 === "3") {
+          if (isBinary(inputValue)) {
+            result = decimalToOctal(parseInt(inputValue, 2));
+          } else {
+            binaryAlert();
+          }
+        } else if (unit1 === "1" && unit2 === "4") {
+          if (isBinary(inputValue)) {
+            result = binaryToHex(inputValue);
+          } else {
+            binaryAlert(); } }
+        else if (unit1 === "2" && unit2 === "1") {
+          result = inputValue.toString(2).padStart(4, "0");
+        } else if (unit1 === "2" && unit2 === "2") {
+          result = inputValue;
+        } else if (unit1 === "2" && unit2 === "3") {
+          result = decimalToOctal(inputValue);
+        } else if (unit1 === "2" && unit2 === "4") {
+          result = decimalToHex(inputValue);
+        } else if (unit1 === "3" && unit2 === "3") {
+          let a = isOctal(inputValue);
+          if (a == true) {
+            result = 'Output-Box';
+          } else {
+            result = inputValue;
+          }
+        } else if (unit1 === "3" && unit2 === "1") {
+          let a = isOctal(inputValue);
+          if (a == true) {
+            result = 'Output-Box';
+          } else {
+            result = octalToBinary(inputValue);
+          } } else if (unit1 === "3" && unit2 === "2") {
+          let a = isOctal(inputValue);
+          if (a == true) {
+            result = 'Output-Box';
+          } else {
+            result = octalToDecimal(inputValue);  }
+        } else if (unit1 === "3" && unit2 === "4") {
+
+          let a = isOctal(inputValue);
+          if (a == true) {
+            result = 'Output-Box';
+          } else {
+            result = octalToHex(inputValue);
+          }
+
+        }
+        else if (unit1 === "4" && unit2 === "1") {
+          console.log(hexValue);
+          if (isHexadecimal(hexValue)) {
+            done();
+            result = hexToBinary(hexValue);
+          } else if (hexValue <= 0) {
+            hexaDecimalWarn();
+          }
+          else {
+            hexaDecimalError();
+          }
+        }
+        else if (unit1 === "4" && unit2 === "2") {
+          if (isHexadecimal(hexValue)) {
+            done();
+            result = hexToDec(hexValue);
+          } else if (hexValue <= 0) {
+            hexaDecimalWarn();
+          }
+          else {
+            hexaDecimalError();
+          }
+        }
+
+        else if (unit1 === "4" && unit2 === "3") {
+          if (isHexadecimal(hexValue)) {
+            result = hexToOct(hexValue);
+            done();
+          } else if (hexValue <= 0) {
+            hexaDecimalWarn();
+          } else {
+            hexaDecimalError();
+          }
+        }
+        else if (unit1 === "4" && unit2 === "4") {
+          if (isHexadecimal(hexValue)) {
+            done();
+            result = hexValue;
+          } else if (hexValue <= 0) {
+            hexaDecimalWarn();
+          } else {
+            hexaDecimalError();
+          }
+        }
       }
-    }
-    else if (unit1 === "4" && unit2 === "2") {
-      if (isHexadecimal(hexValue)) {
-        done();
-        result = hexToDec(hexValue);
-      } else if (hexValue <= 0) {
-        hexaDecimalWarn();
-      }
-      else {
-        hexaDecimalError();
-      }
-    }
-    else if (unit1 === "4" && unit2 === "3") {
-      if (isHexadecimal(hexValue)) {
-        result = hexToOct(hexValue);
-        done();
-      } else if (hexValue <= 0) {
-        hexaDecimalWarn();
-      } else {
-        hexaDecimalError();
-      }
-    }
-    else if (unit1 === "4" && unit2 === "4") {
-      if (isHexadecimal(hexValue)) {
-        done();
-        result = hexValue;
-      } else if (hexValue <= 0) {
-        hexaDecimalWarn();
-      } else {
-        hexaDecimalError();
-      }
-    }
-    answer.innerHTML = result;
-  });
+      answer.innerHTML = result;
+
+    });
+
+  }
 
 })
+
+
+
+convert.addEventListener("click", () => {
+  if ((selectBox1.value == 0 || selectBox2.value == 0) || (selectBox1.value > 0 && selectBox2.value == 0) || (selectBox1.value == 0 && selectBox2.value >= 0)) {
+    appendAlert('Please Select A Type', 'warning', 'coral');
+    result = 'Output-Box';
+
+  }
+})
+
 reset.addEventListener("click", () => {
   location.reload();
 });
